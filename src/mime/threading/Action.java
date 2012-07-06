0445c0a7bd301f4738dbc867f9eb281bfa2728f6
Lead-Programmer2 <Lead-Programmer2@192.168.1.76>
2012-07-06 17:22:38 +0800

    Added mimic for Action class.

--- src/mime/threading/Action.java
+++ src/mime/threading/Action.java
@@ -4,25 +4,62 @@
  */
 package mime.threading;
 
+import javax.swing.SwingWorker;
+
 
 /**
  * Mimics VB.Net Action class.
  * @author seph
  */
-public abstract class Action implements Runnable {
+public class Action {
 
+    private SwingWorker _worker=null;
     private Invokable _invokable=null;
+    private Invokable _callback=null;
     
     /**
      * Creates a new instance of Action.
+     * @param  invokingmethod Method to be invoked.
      */
     public Action(Invokable invokingmethod) {
-        _invokable=invokingmethod;
+        this(invokingmethod, null);
     }
     
-    @Override
-    public void run() {
-        _invokable.invoke();
+    /**
+    * Creates a new instance of Action.
+     * @param  invokingmethod Method to be invoked.
+     * @param callback Callback method that will execute the synchronization is done.
+     */
+    public Action(Invokable invokingmethod, Invokable callback) {
+        _invokable=invokingmethod; _callback=callback;
     }
+    /**
+     * Executes and invokes the initialized Invokable method asynchronously.
+     */
+    public void invoke() {
+        if (_worker!=null) throw new RuntimeException("Thread is already running.");
+        _worker=new SwingWorker() {
+
+            @Override
+            protected Object doInBackground() throws Exception {
+                _invokable.invoke(); return null;
+            }
+            
+            @Override
+            public void done() {
+                if (_callback!=null) _callback.invoke();
+                
+                try {
+                    _worker=null;
+                    finalize(); System.gc();
+                }
+                catch (Throwable ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+        _worker.execute();
+    }
+    
     
 }
