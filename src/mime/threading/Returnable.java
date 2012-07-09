5952cd4a79737f440159f6eb47c50324c4a2897d
Lead-Programmer2 <Lead-Programmer2@192.168.1.76>
2012-07-09 17:13:04 +0800

    Added wrapper interface for Func.

---
+++ src/mime/threading/Returnable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mime.threading;
+
+/**
+ * Interface used to encapsulate a method that returns a value.
+ * @author seph
+ */
+public interface Returnable <T> {
+    
+    public T returns();
+    
+}
