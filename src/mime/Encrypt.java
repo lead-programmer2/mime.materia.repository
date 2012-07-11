fa43a91a4a4c5cffcc7454f77addb0fb6e1e50f2
Lead-Programmer2 <Lead-Programmer2@192.168.1.76>
2012-07-11 09:58:18 +0800

    Added Encrypt static class which encrypts a plain text into a non-special-charactered cipher text.

---
+++ src/mime/Encrypt.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mime;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import sun.misc.BASE64Encoder;
+
+/**
+ * Text encryption class
+ * @author seph
+ */
+public class Encrypt {
+    
+    private static Cipher _cipher;
+
+    private static void init(SecretKey key) {
+        try {
+            _cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+            _cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        } catch (javax.crypto.NoSuchPaddingException ex) { ex.printStackTrace();
+        } catch (java.security.NoSuchAlgorithmException ex) { ex.printStackTrace();
+        } catch (java.security.InvalidKeyException ex) { ex.printStackTrace();
+        }
+    }
+    
+    /**
+     * Generates the encrypted value of the specified text. 
+     * @param value Plain text to be encrypted 
+     * @return Triple-DES-encrypted text representation of the specified value.
+     */
+    public static String valueOf(String value) {
+        return valueOf(value, "mimemateria");
+    }
+    
+    /**
+     * Generates the encrypted value of the specified text. 
+     * @param value Plain text to be encrypted 
+     * @param key Encryption key pattern string.
+     * @return Triple-DES-encrypted text representation of the specified value.
+     */
+    public static String valueOf(String value, String key) { 
+        try {
+             
+            String _key=key;
+            if (_key.length() < 20) _key=key + "additionalkeystring";
+             
+            DESKeySpec _keyspec = new DESKeySpec(_key.getBytes("UTF8"));   
+            SecretKeyFactory _keyfactory=SecretKeyFactory.getInstance("DES");
+              
+            init(_keyfactory.generateSecret(_keyspec));
+              
+            byte[] _utf8 = value.getBytes("UTF8");
+            byte[] _encrypted = _cipher.doFinal(_utf8);
+
+            return new BASE64Encoder().encode(_encrypted);
+        } catch (javax.crypto.BadPaddingException ex) { ex.printStackTrace();
+        } catch (IllegalBlockSizeException ex) { ex.printStackTrace();
+        } catch (UnsupportedEncodingException ex) { ex.printStackTrace();
+        } catch (java.io.IOException ex) { ex.printStackTrace();
+        } catch (NoSuchAlgorithmException ex) { ex.printStackTrace();
+        } catch (InvalidKeySpecException ex) { ex.printStackTrace();
+        } catch (InvalidKeyException ex) { ex.printStackTrace();
+        }
+        
+        return null;
+    }
+}
