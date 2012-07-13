767025c5180def3ec7158ae836a4dedc6c85156b
Lead-Programmer2 <Lead-Programmer2@192.168.1.76>
2012-07-13 09:14:14 +0800

    Re-phrase dispose() method's description.

--- src/mime/data/MySqlConnection.java
+++ src/mime/data/MySqlConnection.java
@@ -126,7 +126,7 @@ public class MySqlConnection {
    
     /**
      * Dispose off any resources used by the class to free up memory space. May throw a RunTimeException especially in cases
-     * of multiple call of this method. To validate whether the current collection already called this method, check isDispose
+     * of multiple call of this method. To validate whether the current class already called this method, check isDispose
      * method.
      */
     public void dispose() {
