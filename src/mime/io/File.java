be1387e6fda666bdcd0c5b58ad61d3fb3f0693f3
Lead-Programmer2 <Lead-Programmer2@192.168.1.76>
2012-07-13 11:26:02 +0800

    Added additional methods create and delete.

--- src/mime/io/File.java
+++ src/mime/io/File.java
@@ -11,6 +11,36 @@ package mime.io;
 public class File {
     
     /**
+     * Creates a file at the specified path.
+     * @param filename Path of the file to be created.
+     * @return StreamWriter associated with the created file.
+     */
+    public static StreamWriter create(String filename) {
+        StreamWriter sw=new StreamWriter(filename);
+        sw.write(""); return sw;
+    }
+    
+    /**
+     * Deletes the specified file.
+     * @param filename Path of the file to be deleted.
+     * @return True if file was successfully deleted, otherwise false.
+     */
+    public static boolean delete(String filename) {
+        boolean _deleted=false;
+        java.io.File _file=new java.io.File(filename);
+        if (_file.exists()) {
+            try {
+                _deleted=_file.delete();
+            }
+            catch (Exception ex) {
+                throw new RuntimeException(ex.getMessage());
+            }
+        }
+        
+        return _deleted;
+    }
+    
+    /**
      * Gets whether the specified file exists or not.
      * @param filename 
      * @return True if the file exist, otherwise false.
