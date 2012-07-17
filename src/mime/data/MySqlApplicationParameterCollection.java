b56cdb386d5d53d02cd11b56fd13118518795b2d
Lead-Programmer2 <Lead-Programmer2@192.168.1.76>
2012-07-17 09:29:40 +0800

    MySql class relative exception

---
+++ src/mime/data/MySqlApplicationParameterCollection.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mime.data;
+
+import java.util.ArrayList;
+
+/**
+ * Collection of MySQL application / dump parameters.
+ * @author seph
+ */
+public class MySqlApplicationParameterCollection extends ArrayList<String> {
+    
+    
+}
